a5ac75afebfc41dd408f70b5840f67e9af4ff190
Kacie Dearman <kldearman@amarillocollege.com>
2026-03-02 19:33:56 -0600

    ♻️ refactor(server): Refine DB and API route config
    *   Updated database connection configuration:
        *   Migrated database URI from `DATABASE_URI` to `VITE_DB_URI` for consistent environment variable usage.
        *   Renamed database pool instance from `db` to `pl`.
    *   Adjusted API routes:
        *   Standardized event-related endpoint parameters from `:eventId` to `:id` for PATCH, PUT, and DELETE operations.
        *   Corrected `getEvents` controller import path.
        *   Refined the `/categories` endpoint path.
    *   Updated server initialization:
        *   Ensured explicit `dotenv` initialization.
        *   Updated default server `PORT` to `3000`.
    
    33/50 commits completed

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getEvents } from '../controllers/getEvent.js';
+import { getEvents } from '../controllers/getEvents.js';
 import { createEvent } from '../controllers/createEvent.js';
 import { modifyEvent } from '../controllers/modifyEvent.js';
 import { toggleFavorite } from '../controllers/toggleFavorite.js';
@@ -23,15 +23,15 @@ route.get('/events', getEvents);
 route.post('/events', createEvent);
 
 // Endpoint for PATCH request to modify an event (modifyEvent.js)
-route.patch('/events/:eventId', modifyEvent);
+route.patch('/events/:id', modifyEvent);
 
 // Endpoint for PUT request to toggle "liked/!liked" on event (toggleFavorite.js)
-route.put('/events/:eventId/favorites', toggleFavorite);
+route.put('/events/:id/favorites', toggleFavorite);
 
 // Endpoint for DELETE request to delete an event (deleteEvent.js)
-route.delete('/events/:eventId', deleteEvent);
+route.delete('/events/:id', deleteEvent);
 
 // Endpoint for GET request for all categories (getCategories.js)
-route.get('/:categoriesId', getCategories);
+route.get('/categories', getCategories);
 
 export default route
--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import route from './routes/routes.js'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
